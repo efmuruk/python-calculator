bc43a4b820b024c534ffc08a548fbf62f3cc8f69
FinchPtay <109161117+efmuruk@users.noreply.github.com>
2024-11-16 18:00:10 +0000

    Added the test cases

--- calculator.py
+++ calculator.py
@@ -1,27 +1,38 @@
 class Calculator:
     def add(self, a, b):
         return a + b
-
+    
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
+        if b < 0 :
+            for i in range(-b):
+                result = self.subtract(result, a)
+        else :
+            for i in range(b):
+                result = self.add(result, a)
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        sign = (a < 0 and b > 0) or (a > 0 and b < 0)
+        b = -b if b < 0 else b
+        a = -a if a < 0 else a
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
-        return result
-    
+        if sign == 1 :
+            return  -result
+        else :
+            return result
+
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
-        return a
+        d = self.divide(a,b)
+        d = d-1 if (a < 0 and b > 0) or (a > 0 and b < 0) else d
+        result = a - self.multiply(d,b)
+        return result
 
 # Example usage:
 if __name__ == "__main__":
